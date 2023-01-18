35640271553af273dfb2f8f8cbe8b8fa7c975898
denios <76059129+deniosm@users.noreply.github.com>
2023-01-18 15:06:13 +0000

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,54 +1,17 @@
-/* HTML document is loaded. DOM is ready.
--------------------------------------------*/
-$(function(){
+// preloader
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+});
 
-    /* start typed element */
-    //http://stackoverflow.com/questions/24874797/select-div-title-text-and-make-array-with-jquery
-    var subElementArray = $.map($('.sub-element'), function(el) { return $(el).text(); });    
-    $(".element").typed({
-        strings: subElementArray,
-        typeSpeed: 30,
-        contentType: 'html',
-        showCursor: false,
-        loop: true,
-        loopCount: true,
-    });
-    /* end typed element */
-
-    /* Smooth scroll and Scroll spy (https://github.com/ChrisWojcik/single-page-nav)    
-    ---------------------------------------------------------------------------------*/ 
+$(function() {
+    new WOW().init();
     $('.templatemo-nav').singlePageNav({
-        offset: $(".templatemo-nav").height(),
-        filter: ':not(.external)',
-        updateHash: false
+    	offset: 70
     });
 
-    /* start navigation top js */
-    $(window).scroll(function(){
-        if($(this).scrollTop()>18){
-            $(".templatemo-nav").addClass("sticky");
-        }
-        else{
-            $(".templatemo-nav").removeClass("sticky");
-        }
-    });
-    
     /* Hide mobile menu after clicking on a link
     -----------------------------------------------*/
     $('.navbar-collapse a').click(function(){
         $(".navbar-collapse").collapse('hide');
     });
-    /* end navigation top js */
-
-    $('body').bind('touchstart', function() {});
-
-    /* wow
-    -----------------*/
-    new WOW().init();
-});
-
-/* start preloader */
-$(window).load(function(){
-	$('.preloader').fadeOut(100); // set duration in brackets    
-});
-/* end preloader */
+})
